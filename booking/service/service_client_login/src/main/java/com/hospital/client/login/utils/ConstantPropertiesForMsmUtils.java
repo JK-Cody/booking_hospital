9ffb7798a150ce20e0fa962dd9ed31496f945e35
JK-Cody <60873845+JK-Cody@users.noreply.github.com>
2022-03-22 14:46:30 +0800

    Update ConstantPropertiesForMsmUtils.java

--- booking/service/service_client_login/src/main/java/com/hospital/client/login/utils/ConstantPropertiesForMsmUtils.java
+++ booking/service/service_client_login/src/main/java/com/hospital/client/login/utils/ConstantPropertiesForMsmUtils.java
@@ -10,20 +10,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class ConstantPropertiesForMsmUtils implements InitializingBean {
 
-//    @Value("${aliyun.sms.regionId}")
+    @Value("${aliyun.sms.regionId}")
     private String regionId;
 
-//    @Value("${aliyun.sms.accessKeyId}")
+   @Value("${aliyun.sms.accessKeyId}")
     private String accessKeyId;
 
-//    @Value("${aliyun.sms.secret}")
+    @Value("${aliyun.sms.secret}")
     private String secret;
 
     public static String REGION_Id;
     public static String ACCESS_KEY_ID;
     public static String SECRECT;
 
-//    取得参数赋值
     @Override
     public void afterPropertiesSet() throws Exception {
         REGION_Id=regionId;
